3e612ed0d8c6dd39792cb63e1028812cbd4fd636
Pedro Neres <bypedroneres@gmail.com>
2023-09-21 10:54:49 -0300

    first 2 classes

---
+++ src/class02/Variables.java
@@ -0,0 +1,29 @@
+package class02;
+
+public class Variables {
+    public static void main (String[] args ) {
+
+        long myLong = 124324;
+        short myShort = 847;
+        int myNumber = 88;
+
+        double myDouble = 23.5;
+        float myFloat = 324.3f;
+
+        char myChar = 'p';
+        boolean myBoolean = false;
+
+        byte myByte = 17;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println("Pedrin da massa");
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+    
+}
