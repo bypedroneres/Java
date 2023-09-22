1523695bff8bf94ac0e95ef8132855201f9368b9
Pedro Neres <bypedroneres@gmail.com>
2023-09-22 08:58:42 -0300

    Finished class05

---
+++ src/class05/ForLoops.java
@@ -0,0 +1,11 @@
+package class05;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for(int i = 0; i < 101; i = i +1) {
+            System.out.printf("The value of i is: %d", i);
+        }
+    }
+}
+// print f is way much cooler than println (my opinion)
+// \n makes a break line just like the /br on HTML
